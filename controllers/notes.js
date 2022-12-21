e0f2b1ae705259eb76d4fe2ca03f1c39c1a15e7b
houssamelm <houssamelmasoudi@gmail.com>
2022-12-21 19:17:37 +0100

    auth

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,29 +1,48 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
 const Note = require('../models/note')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
   response.json(notes)
 })
 
-notesRouter.get('/:id',async (request, response) => {
+notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id)
+
   if (note) {
-    response.json(note)
+    response.json(note.toJSON())
   } else {
     response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body
+notesRouter.post('/', async (request, response) => {
+  const { content, important } = request.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
+    content,
+    important,
     date: new Date(),
     user: user._id
   })
@@ -32,7 +51,7 @@ notesRouter.post('/', async (request, response, next) => {
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
 
-  response.json(savedNote)
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
